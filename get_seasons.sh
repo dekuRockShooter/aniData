f899f601ce2ba57daa04aa9ad913aa15777807dd
dekuRockShooter <dekuRockShooter@gmail.com>
2016-12-23 15:53:37 -0800

    Download charts based on cmd line args.

--- get_seasons.sh
+++ get_seasons.sh
@@ -97,11 +97,35 @@ fi
 
 http_proxy='173.201.183.172:8000'
 
-for year in $(seq 2013 2015);
+#for year in $(seq 2013 2015);
+#do
+    #http_proxy='173.201.183.172:8000'
+    ##wget myanimelist.net/anime/season/$year/spring -O sp$year.html
+    #http_proxy='201.55.46.6:80'
+    #wget myanimelist.net/anime/season/$year/fall -O f$year.html
+    #sleep 90
+#done
+
+for year in $(seq $YEAR_BEG $YEAR_END);
 do
-    http_proxy='173.201.183.172:8000'
-    #wget myanimelist.net/anime/season/$year/spring -O sp$year.html
-    http_proxy='201.55.46.6:80'
-    wget myanimelist.net/anime/season/$year/fall -O f$year.html
-    sleep 90
+    if [ "$GET_WINTER" == 1 ];
+    then
+        wget myanimelist.net/anime/season/$year/winter -O w$year.html
+        sleep 30
+    fi
+    if [ "$GET_SPRING" == 1 ];
+    then
+        wget myanimelist.net/anime/season/$year/spring -O sp$year.html
+        sleep 30
+    fi
+    if [ "$GET_SUMMER" == 1 ];
+    then
+        wget myanimelist.net/anime/season/$year/summer -O su$year.html
+        sleep 30
+    fi
+    if [ "$GET_FALL" == 1 ];
+    then
+        wget myanimelist.net/anime/season/$year/fall -O f$year.html
+        sleep 30
+    fi
 done
