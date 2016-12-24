ec88e128ca1ef4648448bfa5bbeef466b11849eb
dekuRockShooter <dekuRockShooter@gmail.com>
2016-12-24 11:34:17 -0800

    Change help message and fix variable name typos.
    Also, -p does not take an option, so this commit fixes that.

--- anichart.sh
+++ anichart.sh
@@ -19,63 +19,60 @@ ARG_FALL_2=""
 if [ "$#" == 0 ];
 then
     echo -e "\n"\
-         "This program downloads the anime chart for given seasons and years\n"\
-         "from myanimelist.net.  So as to not overload MAL's servers, there\n"\
-         "is a 30 second pause between downloads.  The charts are saved as\n"\
-         "<season><year>.html, where <season> is w, sp, su, or f, depending\n"\
-         "on the arguments given, and <year> is any year between (inclusive)\n"\
-         "the years given in the arguments."
-
-    echo -e "\n\nUsage:\n"\
-         "    ./get_seasons.sh <seasons> <years>\n"\
+         "This program downloads anime data for given seasons of a given\n"\
+         "range of years and inserts them into a database.  It is equivalent"\
+         "\nto this sequence of calls:\n\n"\
+         "    $ ./get_seasons.sh <args>\n"\
+         "    $ ./import_csv_anime.sh <args> -t a\n"\
+         "    $ ./import_csv_anime.sh <args> -t h\n\n"\
+         "where <args> are the arguments to each program that are\n"\
+         "appropriately formatted from the arguments given to anichart.sh.\n"\
+         "\nThe databases into which data are inserted are Sybil.db and\n"\
+         "Hentai.db, both of which must exist in the working directory and\n"\
+         "have a schema usable by aniLog.py."
+    echo -e "Usage:\n"\
+         "    $ ./anichart.sh <seasons> <years>\n"\
          "Both year arguments, and at least one season argument must be given."
 
     echo -e "\n\n<seasons>\n"\
-         "-w    Download the winter anime page\n"\
-         "-p    Download the spring anime page\n"\
-         "-s    Download the summer anime page\n"\
-         "-f    Download the fall anime page"\
+         "-w    Get winter anime data\n"\
+         "-p    Get spring anime data\n"\
+         "-s    Get summer anime data\n"\
+         "-f    Get fall anime data"\
          "\n\n<years>\n"\
-         "-b n  A four digit year, marking the first year to get charts from\n"\
-         "-e n  A four digit year, marking the last year to get charts to"\
+         "-b n  A four digit year, marking the first year to get data from\n"\
+         "-e n  A four digit year, marking the last year to get data to"\
          "\n\nOther:\n"\
          "-h    Show this message\n"
-
-    echo -e "\n\nExample\n"\
-         "Download the charts for winter and fall anime for the years 2010 to"\
-         "2016:\n\n"\
-         "    $ ./get_seasons.sh -w -f -b 2010 -e 2016\n\n"\
-         "The charts are saved in w2010.html, f2010.html, ..., w2016.htm,"\
-         "f2016.html.\n"
     exit
 fi
 
-while getopts "wfsp:b:e:h" opt;
+while getopts "wfspb:e:h" opt;
 do
     case $opt in
         w)
             GET_WINTER=1
             HAS_SEASON_FLAG=1
-            WINTER_ARG="-w"
-            WINTER_ARG_2="w"
+            ARG_WINTER="-w"
+            ARG_WINTER_2="w"
             ;;
         p)
             GET_SPRING=1
             HAS_SEASON_FLAG=1
-            SPRING_ARG="-p"
-            SPRING_ARG_2="sp"
+            ARG_SPRING="-p"
+            ARG_SPRING_2="sp"
             ;;
         s)
             GET_SUMMER=1
             HAS_SEASON_FLAG=1
-            SUMMER_ARG="-s"
-            SUMMER_ARG_2="su"
+            ARG_SUMMER="-s"
+            ARG_SUMMER_2="su"
             ;;
         f)
             GET_FALL=1
             HAS_SEASON_FLAG=1
-            FALL_ARG="-f"
-            FALL_ARG_2="f"
+            ARG_FALL="-f"
+            ARG_FALL_2="f"
             ;;
             #TODO check if it's less than YEAR_END
         b)
@@ -104,8 +101,7 @@ done
 
 if [ "$HAS_SEASON_FLAG" == 0 ];
 then
-    echo "Seasons not specified."\
-         "At least one of -w, -p, -s, and -f must be given."
+    echo "Run with no arguments for help."
     exit
 fi
 
